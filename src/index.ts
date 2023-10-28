a54f3a6b4f69046de67f8f682a86308f35286224
Alexandr Mostovoy <ushanos@gmail.com>
2023-10-28 19:04:24 +0300

    switched from webpack to parcel. Added static-files-copy parcel plugin

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,14 @@ import Game from "./Game";
 const options: object = {
   width: 800,
   height: 600,
+  background: 0x223388,
   antialias: true,
   transparent: true,
 };
 
 const app: PIXI.Application = new PIXI.Application(options);
+
+(window as any)['globalThis']['__PIXI_APP__'] = app;
 
 const init = () => {
   const game = new Game(app);
